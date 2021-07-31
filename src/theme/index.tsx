5525367eb9cdcf0bc7d877570839762eea8c255c
imabdulbasit <baronbasit@gmail.com>
2021-07-31 17:58:25 +0000

    font-update

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -178,12 +178,12 @@ export const TYPE = {
 export const FixedGlobalStyle = createGlobalStyle`
 html, input, textarea, button {
   
-  font-family: 'pleasewritemeasongmedium', sans-serif;
+  font-family: 'jojobaregular', sans-serif;
   font-display: fallback;
 }
 @supports (font-variation-settings: normal) {
   html, input, textarea, button {
-    font-family: 'pleasewritemeasongmedium', sans-serif;
+    font-family: 'jojobaregular', sans-serif;
   }
 }
 
